62e2658c712b3ca24b3734d05226f3b3f093064c
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2018-12-15 15:02:16 +0000

    add README with setup instructions

---
+++ README.md
@@ -0,0 +1,5 @@
+To set up the environment (on a CentOS machine):
+```
+source bin/activate
+source setup_env.sh
+```
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import uproot
 
 def main():
-    pass
+    print("works")
 
 if __name__ == "__main__":
     main()
