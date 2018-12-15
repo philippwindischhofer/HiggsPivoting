8b4e7c8f07946c8269925a4617d619340f8144cc
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2018-12-15 14:59:44 +0000

    initial commit, set up environment

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import uproot
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ setup_env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source /cvmfs/sft.cern.ch/lcg/views/LCG_94python3/x86_64-centos7-gcc8-opt/setup.sh
