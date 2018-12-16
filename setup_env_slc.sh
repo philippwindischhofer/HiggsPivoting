f6d7992717a83b1ec0e5c5a70ee2447a8201c8e9
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2018-12-16 18:13:37 +0000

    removed configuration file for slc. stick with centOS for the time being

--- setup_env_slc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3python3/latest/x86_64-slc6-gcc8-opt/setup.sh
-# for some reason, the Python 3 setup does not (yet) work
-#source /cvmfs/sft.cern.ch/lcg/views/LCG_94python3/x86_64-slc6-gcc8-opt/setup.sh
-#source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc8-opt/setup.sh
