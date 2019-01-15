c66ea7110ac5d2cda8abd4e0e37a39b2b07d0e92
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-01-15 09:49:33 +0000

    removed old script run_adversarial_training.sh

--- run_adversarial_training.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# set up the environment
-source /home/windischhofer/HiggsPivoting/bin/activate
-source /home/windischhofer/HiggsPivoting/setup_env.sh
-
-python /home/windischhofer/HiggsPivoting/TrainAdversarialModel.py --data /home/windischhofer/data/Hbb/training-mc16d.h5 --outdir /home/windischhofer/data/test/Hbb_models/ > /home/windischhofer/data/test/job.log
-
-deactivate
