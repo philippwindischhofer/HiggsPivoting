7caaa9f04980b17dcc4133fb0f4ffd3cbfe74d72
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-03-23 16:08:00 +0000

    have prepared the entire fitting pipeline

--- RunHistFitterCampaign.py
+++ RunHistFitterCampaign.py
@@ -9,11 +9,16 @@ def create_job_script(model_dir, script_dir, training_data_path):
 
     with open(script_path, "w") as outfile:
         outfile.write("#!/bin/bash\n")
-        outfile.write("source /home/windischhofer/HiggsPivoting/bin/activate\n")
-        outfile.write("source /home/windischhofer/HiggsPivoting/setup_env.sh\n")
+        
+        # then, prepare the HistFitter environment
+        outfile.write("PYTHONPATH=/home/windischhofer/HiggsPivoting/fitting" + "\n")
+        outfile.write("source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh" + "\n")
+        outfile.write("lsetup root" + "\n")
+        outfile.write("source /home/windischhofer/HistFitter/v0.61.0/setup.sh" + "\n")
 
-        outfile.write("python /home/windischhofer/HiggsPivoting/ExportAnalysisRegionHistograms.py --data " + training_data_path + " --model_dir " + model_dir + " --out_dir " + model_dir + " --test_size 0.5 " + " > " + os.path.join(model_dir, "fitjob.log") + "\n")
-        outfile.write("deactivate\n")
+        # and launch the Asimov fit
+        outfile.write("cd " + model_dir + "\n")
+        outfile.write('HistFitter.py -w -f -d -D "after,corrMatrix" -F excl -a --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFit.py' + " >> " + os.path.join(model_dir, "fitjob.log") + "\n")
 
     return script_path
 
@@ -22,8 +27,8 @@ def RunHistFitterCampaign(model_dirs):
     
     for model_dir in model_dirs:
         job_script = create_job_script(model_dir, script_dir = model_dir, training_data_path = training_data_path)
-        #CondorJobSubmitter.submit_job(job_script)
-
+        CondorJobSubmitter.submit_job(job_script)
+        
 if __name__ == "__main__":
     model_dirs = sys.argv[1:]
     RunHistFitterCampaign(model_dirs)
