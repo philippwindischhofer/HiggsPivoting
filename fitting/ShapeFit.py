61b350e1ee3f03ae65540fa08327b82411c430a2
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-03-23 14:10:15 +0000

    removed superfluous code

--- fitting/ShapeFit.py
+++ fitting/ShapeFit.py
@@ -91,39 +91,3 @@ if configMgr.executeHistFactory:
     if os.path.isfile("data/{}.root".format(configMgr.analysisName)):
         os.remove("data/{}.root".format(configMgr.analysisName))
 
-###################################################
-
-# # define a separate sample per MC template
-# bkg_sample = Sample("bkg", ROOT.kGreen - 9)
-# bkg_sample.setNormFactor("mu_bkg", 1, 0, 100)
-# bkg_sample.buildHisto([5, 5], "SR1", "mBB", 0.5)
-# bkg_sample.buildHisto([5, 1], "SR2", "mBB", 0.5)
-
-# sig_sample = Sample("sig", ROOT.kPink)
-# sig_sample.setNormFactor("mu_sig", 1, 0, 100)
-# sig_sample.buildHisto([7, 15], "SR1", "mBB", 0.5)
-# sig_sample.buildHisto([7, 15], "SR2", "mBB", 0.5)
-
-# ana = configMgr.addFitConfig("SPlusB")
-
-# meas = ana.addMeasurement(name = "ShapeFit", lumi = 1.0, lumiErr = 0.01)
-# meas.addPOI("mu_sig")
-# meas.addPOI("mu_bkg")
-# meas.addParamSetting("Lumi", True, 1) # no luminosity uncertainty
-
-# chan1 = ana.addChannel("mBB", ["SR1"], 2, 0.5, 2.5)
-# chan1.addSample(bkg_sample)
-# chan1.addSample(sig_sample)
-
-# chan2 = ana.addChannel("mBB", ["SR2"], 2, 0.5, 2.5)
-# chan2.addSample(bkg_sample)
-# chan2.addSample(sig_sample)
-
-# ana.addSignalChannels([chan1, chan2]) # add the signal regions to the fit configuration
-
-# # These lines are needed for the user analysis to run
-# # Make sure file is re-made when executing HistFactory
-# if configMgr.executeHistFactory:
-#     if os.path.isfile("data/%s.root" % configMgr.analysisName):
-#         os.remove("data/%s.root" % configMgr.analysisName) 
-
