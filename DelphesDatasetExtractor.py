a06bb921887127fb68124b75ade0401db7502c24
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-04-06 15:35:09 +0100

    added scripts to run the low-level event selection on the batch system

--- DelphesDatasetExtractor.py
+++ DelphesDatasetExtractor.py
@@ -9,15 +9,8 @@ from configparser import ConfigParser
 from delphes.CrossSectionReader import CrossSectionReader
 from delphes.Hbb0LepDelphesPreprocessor import Hbb0LepDelphesPreprocessor
 
-def PrepareDelphesDataset(input_dir, lumi):
+def PrepareDelphesDataset(xsec_file_path, input_files, lumi):
     """ Return a pandas table with the needed event variables, after applying selection. """
-
-    # look for the file containing the cross section
-    xsec_file_candidates = glob.glob(os.path.join(input_dir, "cross_section.txt"))
-    if len(xsec_file_candidates) != 1:
-        raise Exception("Error: found more than one plausible cross-section file. Please check your files!")
-    else:
-        xsec_file_path = xsec_file_candidates[0]
 
     print("using the following cross-section file: '{}'".format(xsec_file_path))
     metadata = CrossSectionReader.parse(xsec_file_path)
@@ -31,12 +24,8 @@ def PrepareDelphesDataset(input_dir, lumi):
     # look for the ROOT file(s) with the events and process it
     processed_events = []
 
-    print("looking for ROOT files in '{}'".format(input_dir))
-    event_file_candidates = glob.glob(os.path.join(input_dir, "**/*.root"), recursive = True)
-    print("found {} ROOT files for this process".format(len(event_file_candidates)))
-
     pre = Hbb0LepDelphesPreprocessor()
-    for event_file_candidate in event_file_candidates:
+    for event_file_candidate in input_files:
         print("currently processing {}".format(event_file_candidate))
         
         pre.load(event_file_candidate)
@@ -51,54 +40,32 @@ def PrepareDelphesDataset(input_dir, lumi):
 if __name__ == "__main__":
     parser = ArgumentParser(description = "convert Delphes datasets into hdf5, applying some event selection")
     parser.add_argument("--outfile", action = "store", dest = "outfile")
-    parser.add_argument("--config", action = "store", dest = "config")
+    parser.add_argument("--xsec", action = "store", dest = "xsec_file_path")
     parser.add_argument("--lumi", action = "store", dest = "lumi")
+    parser.add_argument("--sname", action = "store", dest = "sample_name")
+    parser.add_argument("files", nargs = '+', action = "store")
     args = vars(parser.parse_args())
 
     outfile_path = args["outfile"]
-    config_file = args["config"]
     lumi = float(args["lumi"])
+    xsec_file_path = args["xsec_file_path"]
+    files = args["files"]
+    sample_name = args["sample_name"]
     
-    # parse the configuration file
-    config = ConfigParser()
-    config.read(config_file)
+    processed_events = []
 
-    for name, section in config.items():
+    processed_events.append(PrepareDelphesDataset(xsec_file_path, files, lumi))
 
-        # check if this section defines a sample
-        sample_re = re.compile("sample:(.+)")
-        m = sample_re.match(name)
+    # merge all events together and dump them
+    merged_events = pd.concat(processed_events).reset_index(drop = True)
+    print("finished processing, here is a sample:")
+    print(merged_events.head())
 
-        if not m:
-            continue
-
-        sample_name = m.group(1)
-
-        print("----------------------------------------------------")
-        print("found configuration for sample '{}'".format(sample_name))
-
-        input_dirs = eval(section["input_dirs"])
-
-        processed_events = []
-
-        for input_dir in input_dirs:
-            print("now processing input '{}'".format(input_dir))
-
-            processed_events.append(PrepareDelphesDataset(input_dir, lumi))
-
-        # merge all events together and dump them
-        merged_events = pd.concat(processed_events).reset_index(drop = True)
-        print("finished processing, here is a sample:")
-        print(merged_events.head())
-
-        print("stored {} events".format(len(merged_events)))
+    print("stored {} events".format(len(merged_events)))
         
-        if os.path.exists(outfile_path):
-            mode = 'a'
-        else:
-            mode = 'w'
-
-        merged_events.to_hdf(outfile_path, key = sample_name, mode = mode)
-
-        print("----------------------------------------------------")
-
+    if os.path.exists(outfile_path):
+        mode = 'a'
+    else:
+        mode = 'w'
+            
+    merged_events.to_hdf(outfile_path, key = sample_name, mode = mode)
---
+++ RunDelphesDatasetExtractorCampaign.py
@@ -0,0 +1,68 @@
+import os, glob, uuid
+from argparse import ArgumentParser
+
+from utils.CondorJobSubmitter import CondorJobSubmitter
+from delphes.CrossSectionReader import CrossSectionReader
+
+def createJobScript(outfile, xsec, lumi, sample_name, input_files, script_dir):
+    sceleton = """
+    #!/bin/bash
+    
+    source /home/windischhofer/HiggsPivoting/bin/activate
+    source /home/windischhofer/HiggsPivoting/setup_env.sh
+
+    python3 /home/windischhofer/HiggsPivoting/DelphesDatasetExtractor.py --xsec {xsec_file_path} --outfile {outfile} --lumi {lumi} --sname {sample_name} {input_files}
+    """
+    
+    opts = {"xsec_file_path": xsec, "outfile": outfile, "lumi": lumi, "sample_name": sample_name, "input_files": " ".join(input_files)}
+
+    script_path = os.path.join(script_dir, str(uuid.uuid4()) + ".sh")
+    with open(script_path, "w") as outfile:
+        outfile.write(sceleton.format(**opts))
+
+    return script_path
+
+def launchDelphesDatasetExtractorJobs(input_dir, output_dir, lumi, sample_name, nfilesperjob):
+    # first, look for the available root files
+    print("looking for ROOT files in '{}'".format(input_dir))
+    event_file_candidates = glob.glob(os.path.join(input_dir, "**/*.root"), recursive = True)
+    print("found {} ROOT files for this process".format(len(event_file_candidates)))
+
+    print("looking for xsec file in '{}'".format(input_dir))
+    # look for the file containing the cross section
+    xsec_file_candidates = glob.glob(os.path.join(input_dir, "cross_section.txt"))
+    if len(xsec_file_candidates) != 1:
+        raise Exception("Error: found more than one plausible cross-section file. Please check your files!")
+    else:
+        xsec_file_path = xsec_file_candidates[0]
+
+    def generate_chunks(inlist, chunksize):
+        return [inlist[cur:cur + chunksize] for cur in range(0, len(inlist), chunksize)]
+
+    chunked_inputs = generate_chunks(event_file_candidates, chunksize = nfilesperjob)
+
+    # generate the jobs acting on these chunked file inputs
+    for job_input in chunked_inputs:
+        cur_outname = str(uuid.uuid4())
+        script_path = createJobScript(outfile = os.path.join(output_dir, cur_outname + ".h5"), xsec = xsec_file_path, lumi = lumi, sample_name = sample_name, input_files = job_input, script_dir = output_dir)
+        CondorJobSubmitter.submit_job(script_path)
+
+if __name__ == "__main__":
+    parser = ArgumentParser("launch batch jobs to apply the event selection")
+    parser.add_argument("--outdir", action = "store", dest = "outdir")
+    parser.add_argument("--lumi", action = "store", dest = "lumi")
+    parser.add_argument("--sname", action = "store", dest = "sample_name")
+    parser.add_argument("indir", nargs = "+", action = "store")
+    parser.add_argument("--nfilesperjob", action = "store", dest = "nfilesperjob")
+    args = vars(parser.parse_args())
+    
+    outdir = args["outdir"]
+    lumi = float(args["lumi"])
+    sample_name = args["sample_name"]
+    nfilesperjob = int(args["nfilesperjob"])
+    indir = args["indir"][0]
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    launchDelphesDatasetExtractorJobs(indir, outdir, lumi, sample_name, nfilesperjob)
