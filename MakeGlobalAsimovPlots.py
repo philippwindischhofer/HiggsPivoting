2c43bd89445d9b4794807a8951bb43d7d8a59ec2
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-05-08 07:52:45 +0100

    removed need to run a sensitivity campaign each time

--- MakeGlobalAsimovPlots.py
+++ MakeGlobalAsimovPlots.py
@@ -10,13 +10,13 @@ def MakeGlobalAsimovPlots(model_dirs, plot_dir):
 
     for model_dir in model_dirs:
         try:
-            with open(os.path.join(model_dir, "hypodict.pkl"), "rb") as fit_infile, open(os.path.join(model_dir, "sensdict.pkl"), "rb") as sens_infile:
+            with open(os.path.join(model_dir, "hypodict.pkl"), "rb") as fit_infile, open(os.path.join(model_dir, "perfdict.pkl"), "rb") as sens_infile:
                 hypodict = pickle.load(fit_infile)
                 sensdict = pickle.load(sens_infile)
                 hypodicts.append(hypodict)
                 sensdicts.append(sensdict)
         except:
-            print("either sensdict.pkl or hypodict.pkl not found for model '{}'".format(model_dir))
+            print("either sensdict.pkl or perfdict.pkl not found for model '{}'".format(model_dir))
 
     # now have all the data, just need to plot it
     PerformancePlotter.plot_asimov_significance_comparison(hypodicts, sensdicts, outdir = plot_dir)
