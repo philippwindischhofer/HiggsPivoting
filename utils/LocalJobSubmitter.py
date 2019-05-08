a01dba9ffa81ce71a08d59271dd3614a527d3c58
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-05-08 07:54:30 +0100

    modify LocalJobSubmitter to allow concurrent local running

--- utils/LocalJobSubmitter.py
+++ utils/LocalJobSubmitter.py
@@ -1,11 +1,20 @@
-import os
+import os, psutil, time
 import subprocess as sp
 
 class LocalJobSubmitter:
 
     @staticmethod
     def submit_job(job_script_path):
+
+        while True:
+            time.sleep(1)
+            cur_utilization = psutil.cpu_percent()
+            if cur_utilization < 90:
+                break
+
         try:
-            sp.check_output(["sh", job_script_path])
+            #sp.check_output(["sh", job_script_path])
+            sp.Popen(["sh", job_script_path])
+            print("launched job")
         except:
             print("Error in child process caught and ignored.")
