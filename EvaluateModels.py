fixed problem with computation of binned significance

EvaluateModels.py
@@ -44,6 +44,8 @@ def main():
     sig_data_test_2j = []
     sig_data_test_3j = []
     sig_aux_test = []
+    sig_aux_test_2j = []
+    sig_aux_test_3j = []
     sig_mBB_test = []
     sig_mBB_test_2j = []
     sig_mBB_test_3j = []
@@ -69,6 +71,8 @@ def main():
         cut_3j = (cur_aux_data[:, TrainingConfig.auxiliary_branches.index("nJ")] == 3)
         
         sig_aux_test.append(cur_aux_data)
+        sig_aux_test_2j.append(cur_aux_data[cut_2j])
+        sig_aux_test_3j.append(cur_aux_data[cut_3j])
         sig_data_test.append(cur_testdata)
         sig_data_test_2j.append(cur_testdata[cut_2j])
         sig_data_test_3j.append(cur_testdata[cut_3j])
@@ -88,6 +92,8 @@ def main():
     bkg_data_test_2j = []
     bkg_data_test_3j = []
     bkg_aux_test = []
+    bkg_aux_test_2j = []
+    bkg_aux_test_3j = []
     bkg_mBB_test = []
     bkg_mBB_test_2j = []
     bkg_mBB_test_3j = []
@@ -113,6 +119,8 @@ def main():
         cut_3j = cur_aux_data[:, TrainingConfig.auxiliary_branches.index("nJ")] == 3
 
         bkg_aux_test.append(cur_aux_data)
+        bkg_aux_test_2j.append(cur_aux_data[cut_2j])
+        bkg_aux_test_3j.append(cur_aux_data[cut_3j])
         bkg_data_test.append(cur_testdata)
         bkg_data_test_2j.append(cur_testdata[cut_2j])
         bkg_data_test_3j.append(cur_testdata[cut_3j])
@@ -149,21 +157,26 @@ def main():
         tsp.plot(outdir = plots_outdir)
 
         # plot the ROC curve as performance measure
-        ev.plot_roc(data_sig = sig_data_test, data_bkg = bkg_data_test, sig_weights = sig_weights_test, bkg_weights = bkg_weights_test, outpath = plots_outdir, aux_sig = sig_aux_test, aux_bkg = bkg_aux_test)
+        ev.plot_roc(data_sig = sig_data_test, data_bkg = bkg_data_test, aux_sig = sig_aux_test, aux_bkg = bkg_aux_test, sig_weights = sig_weights_test, bkg_weights = bkg_weights_test, outpath = plots_outdir, aux_sig = sig_aux_test, aux_bkg = bkg_aux_test)
 
         # generate distortion plots
-        ev.plot_distortion(data_sig = sig_data_test_2j, data_bkg = bkg_data_test_2j, var_sig = sig_mBB_test_2j, var_bkg = bkg_mBB_test_2j, 
+        ev.plot_distortion(data_sig = sig_data_test_2j, data_bkg = bkg_data_test_2j, aux_sig = sig_aux_test_2j, aux_bkg = bkg_aux_test_2j,
+                           var_sig = sig_mBB_test_2j, var_bkg = bkg_mBB_test_2j, 
                            weights_sig = sig_weights_test_2j, weights_bkg = bkg_weights_test_2j, sigeffs = [1.0, 0.5, 0.25], outpath = plots_outdir, 
                            labels_sig = sig_samples, labels_bkg = bkg_samples, xlabel = r'$m_{bb}$ [GeV]', ylabel = "a.u.", path_prefix = "dist_mBB_2j")
-        ev.plot_distortion(data_sig = sig_data_test_3j, data_bkg = bkg_data_test_3j, var_sig = sig_mBB_test_3j, var_bkg = bkg_mBB_test_3j, 
+        ev.plot_distortion(data_sig = sig_data_test_3j, data_bkg = bkg_data_test_3j, aux_sig = sig_aux_test_3j, aux_bkg = bkg_aux_test_3j,
+                           var_sig = sig_mBB_test_3j, var_bkg = bkg_mBB_test_3j, 
                            weights_sig = sig_weights_test_3j, weights_bkg = bkg_weights_test_3j, sigeffs = [1.0, 0.5, 0.25], outpath = plots_outdir, 
                            labels_sig = sig_samples, labels_bkg = bkg_samples, xlabel = r'$m_{bb}$ [GeV]', ylabel = "a.u.", path_prefix = "dist_mBB_3j")
-        ev.plot_distortion(data_sig = sig_data_test_2j, data_bkg = bkg_data_test_2j, var_sig = sig_dRBB_test_2j, var_bkg = bkg_dRBB_test_2j, 
+        ev.plot_distortion(data_sig = sig_data_test_2j, data_bkg = bkg_data_test_2j, aux_sig = sig_aux_test_2j, aux_bkg = bkg_aux_test_2j,
+                           var_sig = sig_dRBB_test_2j, var_bkg = bkg_dRBB_test_2j, 
                            weights_sig = sig_weights_test_2j, weights_bkg = bkg_weights_test_2j, sigeffs = [1.0, 0.5, 0.25], outpath = plots_outdir, 
                            labels_sig = sig_samples, labels_bkg = bkg_samples, xlabel = r'$\Delta R_{bb}$', ylabel = "a.u.", path_prefix = "dist_dRBB_2j", histrange = (0, 5))
-        ev.plot_distortion(data_sig = sig_data_test_3j, data_bkg = bkg_data_test_3j, var_sig = sig_dRBB_test_3j, var_bkg = bkg_dRBB_test_3j, 
+        ev.plot_distortion(data_sig = sig_data_test_3j, data_bkg = bkg_data_test_3j, aux_sig = sig_aux_test_3j, aux_bkg = bkg_aux_test_3j,
+                           var_sig = sig_dRBB_test_3j, var_bkg = bkg_dRBB_test_3j, 
                            weights_sig = sig_weights_test_3j, weights_bkg = bkg_weights_test_3j, sigeffs = [1.0, 0.5, 0.25], outpath = plots_outdir, 
                            labels_sig = sig_samples, labels_bkg = bkg_samples, xlabel = r'$\Delta R_{bb}$', ylabel = "a.u.", path_prefix = "dist_dRBB_3j", histrange = (0, 5))
+
         # ev.plot_distortion(data_sig = sig_data_test, data_bkg = bkg_data_test, var_sig = sig_pTB1_test, var_bkg = bkg_pTB1_test, 
         #                    weights_sig = sig_weights_test, weights_bkg = bkg_weights_test, sigeffs = [1.0, 0.5, 0.25], outpath = plots_outdir, 
         #                    labels_sig = sig_samples, labels_bkg = bkg_samples, xlabel = r'$p_{T, b(1)}$ [GeV]', ylabel = "a.u.", path_prefix = "dist_pTB1")
@@ -177,40 +190,40 @@ def main():
         # plot correlation plots of the classifier with m_BB
         #ev.plot_clf_correlations(varname = "mBB", data_sig = sig_data_test, weights_sig = sig_weights_test, labels_sig = sig_samples, data_bkg = bkg_data_test, weights_bkg = bkg_weights_test, labels_bkg = bkg_samples, outpath = plots_outdir)
 
-        # get inclusive performance metrics and save them
-        perfdict = ev.get_performance_metrics(sig_data_test, bkg_data_test, sig_mBB_test, bkg_mBB_test, sig_weights_test, 
-                                              bkg_weights_test, labels_sig = sig_samples, labels_bkg = bkg_samples)
-        print("got inclusive perfdict = " + str(perfdict))
+        # # get inclusive performance metrics and save them
+        # perfdict = ev.get_performance_metrics(sig_data_test, bkg_data_test, sig_mBB_test, bkg_mBB_test, sig_weights_test, 
+        #                                       bkg_weights_test, labels_sig = sig_samples, labels_bkg = bkg_samples)
+        # print("got inclusive perfdict = " + str(perfdict))
 
-        # get performance metrics for the SR mass range only (from 30 GeV < mBB < 210 GeV)
-        sig_data_test_SR = []
-        bkg_data_test_SR = []
-        sig_mBB_test_SR = []
-        bkg_mBB_test_SR = []
-        sig_weights_test_SR = []
-        bkg_weights_test_SR = []
+        # # get performance metrics for the SR mass range only (from 30 GeV < mBB < 210 GeV)
+        # sig_data_test_SR = []
+        # bkg_data_test_SR = []
+        # sig_mBB_test_SR = []
+        # bkg_mBB_test_SR = []
+        # sig_weights_test_SR = []
+        # bkg_weights_test_SR = []
 
-        for cur_data, cur_mBB, cur_weights in zip(sig_data_test, sig_mBB_test, sig_weights_test):
-            cut_pass = np.logical_and.reduce((cur_mBB > 30, cur_mBB < 210)).flatten()
-            sig_data_test_SR.append(cur_data[cut_pass])
-            sig_mBB_test_SR.append(cur_mBB[cut_pass])
-            sig_weights_test_SR.append(cur_weights[cut_pass])
+        # for cur_data, cur_mBB, cur_weights in zip(sig_data_test, sig_mBB_test, sig_weights_test):
+        #     cut_pass = np.logical_and.reduce((cur_mBB > 30, cur_mBB < 210)).flatten()
+        #     sig_data_test_SR.append(cur_data[cut_pass])
+        #     sig_mBB_test_SR.append(cur_mBB[cut_pass])
+        #     sig_weights_test_SR.append(cur_weights[cut_pass])
 
-        for cur_data, cur_mBB, cur_weights in zip(bkg_data_test, bkg_mBB_test, bkg_weights_test):
-            cut_pass = np.logical_and.reduce((cur_mBB > 30, cur_mBB < 210)).flatten()
-            bkg_data_test_SR.append(cur_data[cut_pass])
-            bkg_mBB_test_SR.append(cur_mBB[cut_pass])
-            bkg_weights_test_SR.append(cur_weights[cut_pass])
+        # for cur_data, cur_mBB, cur_weights in zip(bkg_data_test, bkg_mBB_test, bkg_weights_test):
+        #     cut_pass = np.logical_and.reduce((cur_mBB > 30, cur_mBB < 210)).flatten()
+        #     bkg_data_test_SR.append(cur_data[cut_pass])
+        #     bkg_mBB_test_SR.append(cur_mBB[cut_pass])
+        #     bkg_weights_test_SR.append(cur_weights[cut_pass])
 
-        perfdict_SR = ev.get_performance_metrics(sig_data_test_SR, bkg_data_test_SR, sig_mBB_test_SR, bkg_mBB_test_SR, sig_weights_test_SR, 
-                                                 bkg_weights_test_SR, labels_sig = sig_samples, labels_bkg = bkg_samples, prefix = "SR_")
-        print("got SR perfdict = " + str(perfdict_SR))
+        # perfdict_SR = ev.get_performance_metrics(sig_data_test_SR, bkg_data_test_SR, sig_mBB_test_SR, bkg_mBB_test_SR, sig_weights_test_SR, 
+        #                                          bkg_weights_test_SR, labels_sig = sig_samples, labels_bkg = bkg_samples, prefix = "SR_")
+        # print("got SR perfdict = " + str(perfdict_SR))
 
-        perfdict.update(perfdict_SR)
+        # perfdict.update(perfdict_SR)
 
-        # save the combined perfdict
-        with open(os.path.join(plots_outdir, "perfdict.pkl"), "wb") as outfile:
-           pickle.dump(perfdict, outfile)
+        # # save the combined perfdict
+        # with open(os.path.join(plots_outdir, "perfdict.pkl"), "wb") as outfile:
+        #    pickle.dump(perfdict, outfile)
 
 if __name__ == "__main__":
     main()

analysis/Category.py
@@ -159,27 +159,36 @@ class Category:
 
         outfile.Close()
 
+    # def _get_SB_binning(self, binning, signal_processes, background_processes, var_name):
+    #     # first, bin all participating processes
+    #     binned_signal = []
+    #     binned_background = []
+
+    #     for process_name, events in self.aux_content.items():
+    #         cur_var_values = events[:, TrainingConfig.auxiliary_branches.index(var_name)]
+    #         cur_weights = self.weight_content[process_name]
+
+    #         cur_bin_contents, _ = np.histogram(np.clip(cur_var_values, binning[0], binning[-1]), bins = binning, weights = cur_weights.flatten())
+
+    #         if process_name in signal_processes:
+    #             binned_signal.append(cur_bin_contents)
+    #         elif process_name in background_processes:
+    #             binned_background.append(cur_bin_contents)
+
+    #     # get the total sum of signal- and background events in each bin
+    #     total_binned_signal = np.sum(binned_signal, axis = 0)
+    #     total_binned_background = np.sum(binned_background, axis = 0)
+
+    #     return total_binned_signal, total_binned_background
+
     def _get_SB_binning(self, binning, signal_processes, background_processes, var_name):
-        # first, bin all participating processes
-        binned_signal = []
-        binned_background = []
+        sigvar, sigweights = self.get_event_variable(signal_processes, var_name)
+        bkgvar, bkgweights = self.get_event_variable(background_processes, var_name)
 
-        for process_name, events in self.aux_content.items():
-            cur_var_values = events[:, TrainingConfig.auxiliary_branches.index(var_name)]
-            cur_weights = self.weight_content[process_name]
+        binned_signal, _ = np.histogram(np.clip(sigvar, binning[0], binning[-1]), bins = binning, weights = sigweights.flatten())
+        binned_background, _ = np.histogram(np.clip(bkgvar, binning[0], binning[-1]), bins = binning, weights = bkgweights.flatten())
 
-            cur_bin_contents, _ = np.histogram(np.clip(cur_var_values, binning[0], binning[-1]), bins = binning, weights = cur_weights.flatten())
-
-            if process_name in signal_processes:
-                binned_signal.append(cur_bin_contents)
-            elif process_name in background_processes:
-                binned_background.append(cur_bin_contents)
-
-        # get the total sum of signal- and background events in each bin
-        total_binned_signal = np.sum(binned_signal, axis = 0)
-        total_binned_background = np.sum(binned_background, axis = 0)
-
-        return total_binned_signal, total_binned_background
+        return binned_signal, binned_background
 
     # compute the binned significance of the 'var' distribution of this category to the separation of the 
     # given signal- and background components

plotting/ModelEvaluator.py
@@ -137,7 +137,7 @@ class ModelEvaluator:
 
     # computes a series of performance measures and saves them to a file
     # currently, computes AUROC as robust performance measure, KL as robust fairness measure
-    def get_performance_metrics(self, data_sig, data_bkg, nuis_sig, nuis_bkg, sig_weights, bkg_weights, labels_sig, labels_bkg, sigeffs = [0.5, 0.25], prefix = ""):
+    def get_performance_metrics(self, data_sig, data_bkg, aux_sig, aux_bkg, nuis_sig, nuis_bkg, sig_weights, bkg_weights, labels_sig, labels_bkg, sigeffs = [0.5, 0.25], prefix = ""):
         perfdict = {}
 
         # for performance evaluations that require a binning of the nuisance
@@ -164,7 +164,8 @@ class ModelEvaluator:
         perfdict[prefix + "AUROC"] = auroc
 
         # get the ROC curve
-        fpr, tpr, _ = self.get_roc(np.concatenate(data_sig, axis = 0), np.concatenate(data_bkg, axis = 0), 
+        fpr, tpr, _ = self.get_roc(np.concatenate(data_sig, axis = 0), np.concatenate(data_bkg, axis = 0),
+                                   np.concatenate(aux_sig, axis = 0), np.concatenate(aux_bkg, axis = 0),
                                    np.concatenate(sig_weights, axis = 0), np.concatenate(bkg_weights, axis = 0))
 
         # to get the fairness metrics, need to compute the cut values for the given signal efficiencies
@@ -221,9 +222,9 @@ class ModelEvaluator:
         return perfdict
         
     # compute fpr / tpr of the given data
-    def get_roc(self, data_sig, data_bkg, sig_weights, bkg_weights):
-        pred_sig = self.env.predict(data = data_sig)[:,1]
-        pred_bkg = self.env.predict(data = data_bkg)[:,1]
+    def get_roc(self, data_sig, data_bkg, aux_sig, aux_bkg, sig_weights, bkg_weights):
+        pred_sig = self.env.predict(data = data_sig, auxdat = aux_sig)[:,1]
+        pred_bkg = self.env.predict(data = data_bkg, auxdat = aux_bkg)[:,1]
 
         pred = np.concatenate([pred_sig, pred_bkg], axis = 0)
         weights = np.concatenate([sig_weights, bkg_weights], axis = 0)
@@ -235,7 +236,7 @@ class ModelEvaluator:
         return fpr, tpr, auc
 
     # plot the ROC of the classifier
-    def plot_roc(self, data_sig, data_bkg, sig_weights, bkg_weights, outpath, aux_sig, aux_bkg):
+    def plot_roc(self, data_sig, data_bkg, aux_sig, aux_bkg, sig_weights, bkg_weights, outpath):
         # need to merge all signal- and background samples for the inclusive ROC
         data_sig_inclusive = np.concatenate(data_sig, axis = 0)
         data_bkg_inclusive = np.concatenate(data_bkg, axis = 0)
@@ -252,6 +253,8 @@ class ModelEvaluator:
         cut_bkg_central = np.logical_and(mBB_bkg > 100.0, mBB_bkg < 150.0)
         data_sig_central = data_sig_inclusive[cut_sig_central]
         data_bkg_central = data_bkg_inclusive[cut_bkg_central]
+        aux_sig_central = aux_sig_inclusive[cut_sig_central]
+        aux_bkg_central = aux_bkg_inclusive[cut_bkg_central]
         sig_weights_central = sig_weights_inclusive[cut_sig_central]
         bkg_weights_central = bkg_weights_inclusive[cut_bkg_central]
 
@@ -260,6 +263,8 @@ class ModelEvaluator:
         cut_bkg_high = mBB_bkg > 150.0
         data_sig_high = data_sig_inclusive[cut_sig_high]
         data_bkg_high = data_bkg_inclusive[cut_bkg_high]
+        aux_sig_high = aux_sig_inclusive[cut_sig_high]
+        aux_bkg_high = aux_bkg_inclusive[cut_bkg_high]
         sig_weights_high = sig_weights_inclusive[cut_sig_high]
         bkg_weights_high = bkg_weights_inclusive[cut_bkg_high]
 
@@ -268,6 +273,8 @@ class ModelEvaluator:
         cut_bkg_low = mBB_bkg < 100.0
         data_sig_low = data_sig_inclusive[cut_sig_low]
         data_bkg_low = data_bkg_inclusive[cut_bkg_low]
+        aux_sig_low = aux_sig_inclusive[cut_sig_low]
+        aux_bkg_low = aux_bkg_inclusive[cut_bkg_low]
         sig_weights_low = sig_weights_inclusive[cut_sig_low]
         bkg_weights_low = bkg_weights_inclusive[cut_bkg_low]
 
@@ -288,10 +295,11 @@ class ModelEvaluator:
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
-        for cur_data_sig, cur_data_bkg, cur_sig_weights, cur_bkg_weights, color, label_base in zip([data_sig_inclusive, data_sig_central, data_sig_high, data_sig_low], [data_bkg_inclusive, data_bkg_central, data_bkg_high, data_bkg_low],
+        for cur_data_sig, cur_data_bkg, cur_aux_sig, cur_aux_bkg, cur_sig_weights, cur_bkg_weights, color, label_base in zip([data_sig_inclusive, data_sig_central, data_sig_high, data_sig_low], [data_bkg_inclusive, data_bkg_central, data_bkg_high, data_bkg_low],
+                                                                                                                             [aux_sig_inclusive, aux_sig_central, aux_sig_high, aux_sig_low], [aux_bkg_inclusive, aux_bkg_central, aux_bkg_high, aux_bkg_low],
                                                                                                    [sig_weights_inclusive, sig_weights_central, sig_weights_high, sig_weights_low], [bkg_weights_inclusive, bkg_weights_central, bkg_weights_high, bkg_weights_low],
                                                                                                    colors, label_bases):
-            fpr, tpr, auc = self.get_roc(cur_data_sig, cur_data_bkg, cur_sig_weights, cur_bkg_weights)
+            fpr, tpr, auc = self.get_roc(cur_data_sig, cur_data_bkg, cur_aux_sig, cur_aux_bkg, cur_sig_weights, cur_bkg_weights)
             ax.plot(tpr, fpr, color = color, label = label_base + r' (AUC = {:.3f})'.format(auc))
 
         ax.set_xlabel("signal efficiency")
@@ -422,9 +430,9 @@ class ModelEvaluator:
         fig.savefig(outpath)
         plt.close()
 
-    def plot_distortion(self, data_sig, data_bkg, var_sig, var_bkg, weights_sig, weights_bkg, sigeffs, outpath, labels_sig = None, labels_bkg = None, num_cols = 2, xlabel = r'$m_{bb}$ [GeV]', ylabel = 'a.u.', path_prefix = "dist_mBB", histrange = (0, 500)):
-        pred_bkg = [self.env.predict(data = sample)[:,1] for sample in data_bkg]
-        pred_sig = [self.env.predict(data = sample)[:,1] for sample in data_sig]
+    def plot_distortion(self, data_sig, data_bkg, aux_sig, aux_bkg, var_sig, var_bkg, weights_sig, weights_bkg, sigeffs, outpath, labels_sig = None, labels_bkg = None, num_cols = 2, xlabel = r'$m_{bb}$ [GeV]', ylabel = 'a.u.', path_prefix = "dist_mBB", histrange = (0, 500)):
+        pred_bkg = [self.env.predict(data = sample, auxdat = aux)[:,1] for sample, aux in zip(data_bkg, aux)]
+        pred_sig = [self.env.predict(data = sample, auxdat = aux)[:,1] for sample, aux in zip(data_sig, aux)]
         
         # create the output directory if it doesn't already exist and save the figure(s)
         if not os.path.exists(outpath):