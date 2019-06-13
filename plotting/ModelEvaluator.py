enforce correct normalization for JSD computation

plotting/ModelEvaluator.py
@@ -55,9 +55,9 @@ class ModelEvaluator:
         p_binned = np.maximum(p_binned, 0.0)
         q_binned = np.maximum(q_binned, 0.0)
 
-        # scale it up by the bin width to get actual per-bin probabilities instead of a density
-        p_binned *= (binning[1] - binning[0])
-        q_binned *= (binning[1] - binning[0])
+        # renormalize
+        p_binned /= np.sum(p_binned)
+        q_binned /= np.sum(q_binned)
 
         # this code is taken (almost) verbatim from https://github.com/scipy/scipy/blob/c42462a/scipy/spatial/distance.py#L1239-L1296
         m_binned = (p_binned + q_binned) / 2.0