b060ff1b7068de90c0b943b486c4164d823999d3
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-06-24 11:29:36 +0100

    removed all hard-coded absolute paths from RunHistFitterCampaign

--- RunHistFitterCampaign.py
+++ RunHistFitterCampaign.py
@@ -4,7 +4,7 @@ from utils.CondorJobSubmitter import CondorJobSubmitter
 from utils.LocalJobSubmitter import LocalJobSubmitter
 from base.Configs import TrainingConfig
 
-def create_job_script(model_dir, script_dir, training_data_path):
+def create_job_script(model_dir, script_dir, training_data_path, rootdir = "", histfitter_rootdir = ""):
     script_name = str(uuid.uuid4()) + ".sh"
     script_path = os.path.join(script_dir, script_name)
     plot_dir = model_dir
@@ -13,7 +13,7 @@ def create_job_script(model_dir, script_dir, training_data_path):
         outfile.write("#!/bin/bash\n")
         
         # then, prepare the HistFitter environment
-        outfile.write("export PYTHONPATH=/home/windischhofer/HiggsPivoting/fitting:$PYTHONPATH" + "\n")
+        outfile.write("export PYTHONPATH=" + os.path.join(rootdir, "fitting") + ":$PYTHONPATH" + "\n")
         outfile.write("export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase" + "\n")
         outfile.write("export ALRB_rootVersion=6.14.04-x86_64-slc6-gcc62-opt" + "\n")
         outfile.write("source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh" + "\n")
@@ -22,26 +22,26 @@ def create_job_script(model_dir, script_dir, training_data_path):
 
         # and launch the Asimov fits
         outfile.write("cd " + model_dir + "\n")
-        outfile.write('/home/windischhofer/HistFitter/v0.61.0/scripts/HistFitter.py -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitNCategoriesBackgroundFixed.py' + " >> " + os.path.join(model_dir, "fitjob_tight_loose_background_fixed.log") + "\n")
-        outfile.write('/home/windischhofer/HistFitter/v0.61.0/scripts/HistFitter.py -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitNCategoriesBackgroundFloating.py' + " >> " + os.path.join(model_dir, "fitjob_tight_loose_background_floating.log") + "\n")
+        outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitNCategoriesBackgroundFixed.py' + " >> " + os.path.join(model_dir, "fitjob_tight_loose_background_fixed.log") + "\n")
+        outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitNCategoriesBackgroundFloating.py' + " >> " + os.path.join(model_dir, "fitjob_tight_loose_background_floating.log") + "\n")
 
         # also launch Asimov fits in the case of the cut-based analysis
-        outfile.write('/home/windischhofer/HistFitter/v0.61.0/scripts/HistFitter.py -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFixed.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_fixed.log") + "\n")
-        outfile.write('/home/windischhofer/HistFitter/v0.61.0/scripts/HistFitter.py -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFloating.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_floating.log") + "\n")
+        outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFixed.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_fixed.log") + "\n")
+        outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFloating.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_floating.log") + "\n")
 
         # also launch Asimov fits in the case of the cut-based analysis
-        outfile.write('/home/windischhofer/HistFitter/v0.61.0/scripts/HistFitter.py -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFixedOriginal.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_fixed_original.log") + "\n")
-        outfile.write('/home/windischhofer/HistFitter/v0.61.0/scripts/HistFitter.py -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFloatingOriginal.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_floating_original.log") + "\n")
+        outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFixedOriginal.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_fixed_original.log") + "\n")
+        outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFloatingOriginal.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_floating_original.log") + "\n")
 
         # the Asimov significances that have been determined above
-        outfile.write("python /home/windischhofer/HiggsPivoting/fitting/ConvertHistFitterResult.py --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFixed_hypotest.root") + " --outkey asimov_sig_ncat_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-        outfile.write("python /home/windischhofer/HiggsPivoting/fitting/ConvertHistFitterResult.py --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFloating_hypotest.root") + " --outkey asimov_sig_ncat_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFixed_hypotest.root") + " --outkey asimov_sig_ncat_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py" + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFloating_hypotest.root") + " --outkey asimov_sig_ncat_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
 
         # the equivalent results for the cut-based setup
-        outfile.write("python /home/windischhofer/HiggsPivoting/fitting/ConvertHistFitterResult.py --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFixedOriginal_hypotest.root") + " --outkey original_asimov_sig_high_low_MET_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-        outfile.write("python /home/windischhofer/HiggsPivoting/fitting/ConvertHistFitterResult.py --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFloatingOriginal_hypotest.root") + " --outkey original_asimov_sig_high_low_MET_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-        outfile.write("python /home/windischhofer/HiggsPivoting/fitting/ConvertHistFitterResult.py --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFixedOptimized_hypotest.root") + " --outkey optimized_asimov_sig_high_low_MET_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-        outfile.write("python /home/windischhofer/HiggsPivoting/fitting/ConvertHistFitterResult.py --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFloatingOptimized_hypotest.root") + " --outkey optimized_asimov_sig_high_low_MET_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFixedOriginal_hypotest.root") + " --outkey original_asimov_sig_high_low_MET_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFloatingOriginal_hypotest.root") + " --outkey original_asimov_sig_high_low_MET_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFixedOptimized_hypotest.root") + " --outkey optimized_asimov_sig_high_low_MET_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFloatingOptimized_hypotest.root") + " --outkey optimized_asimov_sig_high_low_MET_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
 
     return script_path
 
@@ -54,5 +54,11 @@ def RunHistFitterCampaign(model_dirs):
         LocalJobSubmitter.submit_job(job_script)
         
 if __name__ == "__main__":
+    if not os.environ["ROOTDIR"] or not os.environ["HISTFITTER_ROOTDIR"]:
+        raise Exception("Error: 'ROOTDIR' or 'HISTFITTER_ROOTDIR' not defined. Please do 'source setup_env.sh'.")
+
+    rootdir = os.environ["ROOTDIR"]
+    histfitter_rootdir = os.environ["HISTFITTER_ROOTDIR"]
+
     model_dirs = sys.argv[1:]
-    RunHistFitterCampaign(model_dirs)
+    RunHistFitterCampaign(model_dirs, rootdir, histfitter_rootdir)
--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# keep track of the root path for the codebase
+export ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export HISTFITTER_ROOTDIR="/home/windischhofer/HistFitter/v0.61.0"
 # for the moment, need to use the most recent nightly build
 source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3python3/Mon/x86_64-centos7-gcc62-opt/setup.sh
 #source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3python3/Fri/x86_64-centos7-gcc62-opt/setup.sh
