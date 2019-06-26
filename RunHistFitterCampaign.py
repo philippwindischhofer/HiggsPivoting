ec662aebe36a4977a09aeefaa23e3cd19e08d58c
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2019-06-26 11:16:51 +0100

    modified further orchestration scripts to be more universal

--- RunHistFitterCampaign.py
+++ RunHistFitterCampaign.py
@@ -19,39 +19,39 @@ def create_job_script(model_dir, script_dir, training_data_path, rootdir = "", h
         outfile.write("source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh" + "\n")
         outfile.write("lsetup root" + "\n")
         outfile.write("source " + os.path.join(histfitter_rootdir, "setup.sh") + "\n")
-
+        
         # and launch the Asimov fits
         outfile.write("cd " + model_dir + "\n")
         outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitNCategoriesBackgroundFixed.py' + " >> " + os.path.join(model_dir, "fitjob_tight_loose_background_fixed.log") + "\n")
         outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitNCategoriesBackgroundFloating.py' + " >> " + os.path.join(model_dir, "fitjob_tight_loose_background_floating.log") + "\n")
-
+        
         # also launch Asimov fits in the case of the cut-based analysis
         outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFixed.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_fixed.log") + "\n")
         outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFloating.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_floating.log") + "\n")
-
+        
         # also launch Asimov fits in the case of the cut-based analysis
         outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFixedOriginal.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_fixed_original.log") + "\n")
         outfile.write(os.path.join(histfitter_rootdir, "scripts", "HistFitter.py") + ' -w -f -d -D allPlots -F disc -m ALL -a -z --userArg ' + model_dir + ' /home/windischhofer/HiggsPivoting/fitting/ShapeFitHighLowMETBackgroundFloatingOriginal.py' + " >> " + os.path.join(model_dir, "fitjob_high_low_MET_background_floating_original.log") + "\n")
-
+        
         # the Asimov significances that have been determined above
         outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFixed_hypotest.root") + " --outkey asimov_sig_ncat_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py" + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFloating_hypotest.root") + " --outkey asimov_sig_ncat_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-
+        outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitNCategoriesBackgroundFloating_hypotest.root") + " --outkey asimov_sig_ncat_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
+        
         # the equivalent results for the cut-based setup
         outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFixedOriginal_hypotest.root") + " --outkey original_asimov_sig_high_low_MET_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
         outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFloatingOriginal_hypotest.root") + " --outkey original_asimov_sig_high_low_MET_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
         outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFixedOptimized_hypotest.root") + " --outkey optimized_asimov_sig_high_low_MET_background_fixed" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
         outfile.write("python " + os.path.join(rootdir, "fitting", "ConvertHistFitterResult.py") + " --mode hypotest --infile " + os.path.join(model_dir, "results", "ShapeFitHighLowMETBackgroundFloatingOptimized_hypotest.root") + " --outkey optimized_asimov_sig_high_low_MET_background_floating" + " --outfile " + os.path.join(model_dir, "hypodict.pkl") + "\n")
-
+        
     return script_path
 
 def RunHistFitterCampaign(model_dirs):
     training_data_path = TrainingConfig.data_path
     
     for model_dir in model_dirs:
-        job_script = create_job_script(model_dir, script_dir = model_dir, training_data_path = training_data_path)
-        #CondorJobSubmitter.submit_job(job_script)
-        LocalJobSubmitter.submit_job(job_script)
+        job_script = create_job_script(model_dir, script_dir = model_dir, training_data_path = training_data_path, 
+                                       rootdir = os.environ["ROOTDIR"], histfitter_rootdir = os.environ["HISTFITTER_ROOTDIR"])
+        TrainingConfig.submitter.submit_job(job_script)
         
 if __name__ == "__main__":
     if not os.environ["ROOTDIR"] or not os.environ["HISTFITTER_ROOTDIR"]:
--- RunPrepareHistFitterCampaign.py
+++ RunPrepareHistFitterCampaign.py
@@ -5,18 +5,18 @@ from utils.CondorJobSubmitter import CondorJobSubmitter
 from utils.LocalJobSubmitter import LocalJobSubmitter
 from base.Configs import TrainingConfig
 
-def create_job_script(model_dir, script_dir, training_data_path, use_test = False):
+def create_job_script(model_dir, script_dir, training_data_path, rootdir, use_test = False):
     script_name = str(uuid.uuid4()) + ".sh"
     script_path = os.path.join(script_dir, script_name)
     plot_dir = model_dir
 
     with open(script_path, "w") as outfile:
         outfile.write("#!/bin/bash\n")
-        outfile.write("source /home/windischhofer/HiggsPivoting/bin/activate\n")
-        outfile.write("source /home/windischhofer/HiggsPivoting/setup_env.sh\n")
+        outfile.write("source " + os.path.join(rootdir, "bin", "activate") + "\n")
+        outfile.write("source " + os.path.join(rootdir, "setup_env.sh") + "\n")
 
         # first, generate the MC templates
-        outfile.write("python /home/windischhofer/HiggsPivoting/ExportAnalysisRegionHistograms.py --data " + training_data_path + " --model_dir " + model_dir + " --out_dir " + model_dir + (" --use_test " if use_test else "") + " > " + os.path.join(model_dir, "fitjob.log") + "\n")
+        outfile.write("python " + os.path.join(rootdir, "ExportAnalysisRegionHistograms.py") + " --data " + training_data_path + " --model_dir " + model_dir + " --out_dir " + model_dir + (" --use_test " if use_test else "") + " > " + os.path.join(model_dir, "fitjob.log") + "\n")
         outfile.write("deactivate\n")
         
     return script_path
@@ -25,11 +25,13 @@ def RunPrepareHistFitterCampaign(model_dirs, **kwargs):
     training_data_path = TrainingConfig.data_path
     
     for model_dir in model_dirs:
-        job_script = create_job_script(model_dir, script_dir = model_dir, training_data_path = training_data_path, **kwargs)
-        #CondorJobSubmitter.submit_job(job_script)
-        LocalJobSubmitter.submit_job(job_script)
+        job_script = create_job_script(model_dir, script_dir = model_dir, training_data_path = training_data_path, rootdir = os.environ["ROOTDIR"], **kwargs)
+        TrainingConfig.submitter.submit_job(job_script)
 
 if __name__ == "__main__":
+    if not os.environ["ROOTDIR"]:
+        raise Exception("Error: 'ROOTDIR' not defined. Please do 'source setup_env.sh'.")
+
     parser = ArgumentParser()
     parser.add_argument("--use_test", action = "store_const", const = True, default = False, dest = "use_test")
     parser.add_argument("model_dirs", nargs = '+', action = "store")
