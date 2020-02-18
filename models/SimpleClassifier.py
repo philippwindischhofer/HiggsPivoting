e096dd672278d6d6092f56fb50e93927d91e621c
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2020-02-18 12:23:44 +0000

    fix bug in deterministic classifier

--- models/SimpleClassifier.py
+++ models/SimpleClassifier.py
@@ -1,32 +1,35 @@
 import tensorflow as tf
+#import tensorflow_probability as tfp
 import tensorflow.contrib.layers as layers
 
 from models.BaseModels import ClassifierModel
 
 class SimpleClassifier(ClassifierModel):
     def __init__(self, name, hyperpars):
-        print("initializing a SimpleClassifier")
         self.name = name
         self.hyperpars = hyperpars
 
     def build_model(self, in_tensor, is_training):
+
         with tf.variable_scope(self.name):
             lay = in_tensor
 
             for layer in range(int(float(self.hyperpars["num_hidden_layers"]))):
-                lay = layers.relu(lay, int(float(self.hyperpars["num_units"])))
+                lay = layers.relu(lay, int(float(self.hyperpars["num_units"])), weights_initializer = layers.xavier_initializer(seed = 12345),
+                                  weights_regularizer = layers.l2_regularizer(scale = 1.0))
                 lay = layers.dropout(lay, keep_prob = 1 - float(self.hyperpars["dropout_rate"]), is_training = is_training)
 
-            lay = layers.relu(lay, 2)
-            outputs = layers.softmax(lay)
+            pre_output = layers.linear(lay, 1)            
+            normsample = tf.math.sigmoid(pre_output)
+            outputs = tf.concat([normsample, 1 - normsample], axis = 1)
 
         these_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = self.name)
     
-        return outputs, these_vars
+        return outputs, these_vars, pre_output
 
     def build_loss(self, pred, labels_one_hot, weights = 1.0, batchnum = 0):
-        classification_loss = tf.losses.softmax_cross_entropy(onehot_labels = labels_one_hot,
-                                                              logits = pred, 
-                                                              weights = weights)
+        with tf.variable_scope(self.name):
+            classification_loss = tf.losses.softmax_cross_entropy(onehot_labels = labels_one_hot,
+                                                                  logits = pred,
+                                                                  weights = weights)
         return classification_loss
-        
