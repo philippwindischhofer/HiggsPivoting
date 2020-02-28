72aa30f4df2f34563782ca1f2c8241341d8d065f
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2020-02-28 15:09:46 +0000

    setup update

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # keep track of the root path for the codebase
 export ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#export HISTFITTER_ROOTDIR="PATH_TO_HISTFITTER_INSTALLATION_DIR"
-#export PYTHONPATH="PATH_TO_ROOT_BUILD_DIR/lib":$PYTHONPATH
+export HISTFITTER_ROOTDIR="/home/windischhofer/HistFitter/v0.61.0"
+export PYTHONPATH="/home/windischhofer/HiggsPivotingPaperComments/ROOT/root-bin/lib":$PYTHONPATH
